7c43039226284cc1e8d91a7a05b46ea384b60b57
SkippingVenus <lopeztarancoandre@gmail.com>
2026-05-01 23:31:08 -0500

    cambio pequeño en front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: '0.0.0.0',
     port: 5173,
+    allowedHosts: 'all',
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: process.env.VITE_API_URL || 'http://localhost:8000',
         changeOrigin: true,
       },
     },
